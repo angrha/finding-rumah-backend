d93ec24f401f117a8172aeb19e39079fd19d1625
angrha <angrha@gmail.com>
2018-01-16 15:12:59 +0700

    first commit, add user

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router  = express.Router();
+const User    = require('../controllers/userController')
+const isLogin = require('../helper/authentication')
+const isAdmin = require('../helper/authorizationAdmin')
+
+router.get('/', User.findAll)
+router.post('/', User.createUser)
+router.put('/:id', User.updateUser)
+router.delete('/:id', isAdmin, User.deleteUser)
+router.post('/signup', User.register)
+router.post('/signin', User.login)
+
+
+module.exports = router;
